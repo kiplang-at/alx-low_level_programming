a683aefd6e460ea1142ad184d80675ffbad918bb
kiplang-at <linouskt@outlook.com>
2024-01-09 10:12:41 -0800

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+  * main - Entry point of the program
+  * Description: This program uses the function puts to print the specific
+  * string and then returns 0.
+  * Return: 0 on successful execution
+  */
+int main(void)
+{
+	/* print the string using puts */
+	puts("\"Programming is like building a multilingual puzzle");
+
+	/* Return 0 to indicate successful execution */
+	return (0);
+}
+

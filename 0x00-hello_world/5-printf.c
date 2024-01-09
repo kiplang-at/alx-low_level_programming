02a0383c1a1141af2b485806d75338f2f41852c3
kiplang-at <linouskt@outlook.com>
2024-01-09 11:44:16 -0800

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+  * main - Entry point of the program
+  * Description: Prints 'with proper grammer, but the outcome is a piece
+  * of art using the printf function
+  * Return: 0 on successful execution
+  */
+int main(void)
+{
+	/* print the string using printf*/
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	/* Return 0 to indicate successful execution */
+	return (0);
+}

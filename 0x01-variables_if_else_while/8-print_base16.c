81734756d1c5f64ead6e522551f1545ca1c71e3b
kiplang-at <linouskt@outlook.com>
2024-02-06 04:09:55 -0800

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+  * main - Entry point of the program.
+  * Description: Prints all the numbers of base 16 in lowercase,
+  * followed by a new line.
+  * Return: Always returns 0 on sucessfull execution.
+  */
+int main(void)
+{
+	char hex_digit;
+
+	for (hex_digit = '0'; hex_digit <= '9'; hex_digit++)
+	{
+		putchar(hex_digit);
+	}
+	for (hex_digit = 'a'; hex_digit <= 'f'; hex_digit++)
+	{
+		putchar(hex_digit);
+	}
+	putchar('\n');
+	return (0);
+}
